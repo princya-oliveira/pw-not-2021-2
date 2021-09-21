06a124b876af0036f35f84c49b952bc3cca20d30
princya-oliveira <princya.oliveira@outlook.com>
2021-09-21 23:28:27 +0000

    (21/09) exercício 03 e 04

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -3,6 +3,8 @@ import Teste1 from './exercicios/Teste1'
 import Teste2 from './exercicios/Teste2'
 import Exercicio01 from './exercicios/01'
 import Exercicio02 from './exercicios/02'
+import Exercicio03 from './exercicios/03'
+import Exercicio04 from './exercicios/04'
 
 import './App.css';
 
@@ -20,6 +22,8 @@ function App() {
           <li><Link to="teste2">Teste 2</Link></li>
           <li><Link to="ex01">Exercício 01</Link></li>
           <li><Link to="ex02">Exercício 02</Link></li>
+          <li><Link to="ex03">Exercício 03</Link></li>
+          <li><Link to="ex04">Exercício 04</Link></li>
         </ul>
 
         <div style={{border: '1px solid blue', padding: '20px' }}>
@@ -28,6 +32,8 @@ function App() {
             <Route path="/teste2"><Teste2 /></Route>
             <Route path="/ex01"><Exercicio01 /></Route>
             <Route path="/ex02"><Exercicio02 /></Route>
+            <Route path="/ex03"><Exercicio03 /></Route>
+            <Route path="/ex04"><Exercicio04 /></Route>
           </Switch>
         </div>
       </BrowserRouter>
