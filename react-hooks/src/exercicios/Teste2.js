44c7d41dcba4f812b9c0ee6d0e44154b00d9ee55
princya-oliveira <princyanoliveira@gmail.com>
2021-10-05 22:38:34 +0000

    (05/10) correção prova1 + estados objeto

--- react-hooks/src/exercicios/Teste2.js
+++ react-hooks/src/exercicios/Teste2.js
@@ -1,11 +1,12 @@
 import * as React from 'react'
 
 export default function Teste2() {
-    const [caption, setCaption] = React.useState('Clique aqui')
 
-    const handleButtonClick = event => setCaption('Botão clicado')
-    
-    return (
-        <button onClick={handleButtonClick}>{caption}</button>
-    )
+  const [caption, setCaption] = React.useState('Clique aqui')
+
+  const handleButtonClick = event => setCaption('Botão clicado') 
+
+  return (
+    <button onClick={handleButtonClick}>{caption}</button>
+  )
 }
